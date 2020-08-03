238e96198c5e3e40d962b97cccf1daf3f32dcb61
Daniel Gould <dmgould310@gmail.com>
2020-08-03 17:34:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 # Your code goes here!
-class Dog 
+
   
-  def name (name)
-    @name = name 
-  end 
-  def bark 
+class Dog
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark
     puts "woof!"
-  end 
-  
-end 
+  end
+
+
+end
